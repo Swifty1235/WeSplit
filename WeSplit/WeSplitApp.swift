37ab1246113555b0a251cfde70109a756205ebcb
Pedro Romero <104468694+Swifty1235@users.noreply.github.com>
2024-06-11 18:18:46 -0400

    Add files via upload
    Deleted and added again. Finished product of WeSplit. Day 16-18 SwiftUI Course

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Pedro Romero on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
